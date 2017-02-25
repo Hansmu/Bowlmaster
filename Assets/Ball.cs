9a203a6e0f5dbb87f2b4580a9309d538846d4063
hansmu <hans.miikael@gmail.com>
2017-02-25 15:18:38 +0200

    A ball.

---
+++ Assets/Ball.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	public float launchSpeed;
+
+	private Rigidbody rigidBody;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		rigidBody.velocity = new Vector3(0, 0, launchSpeed);
+	
+		LaunchBall();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LaunchBall() {
+		audioSource = GetComponent<AudioSource>();
+		audioSource.Play();
+	}
+}
